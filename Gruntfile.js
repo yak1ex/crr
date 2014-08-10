62d257da23f11cd1d0c6281706fe4451e7d4f81f
Yasutaka ATARASHI <yak_ex@mx.scn.tv>
2014-08-10 20:14:15 +0900

    Add release task by Grunt (close gh-5, gh-12).

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,42 @@
 module.exports = function(grunt) {
 
-// concat
+// load plugins
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.config('concat', {
-    scripts: {
-      src: [
-        'bower_components/jquery/dist/jquery.js',
-        'bower_components/mathjs/dist/math.js',
-        'crrlib.js'
-      ],
-      dest: 'tmp/crr.js'
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+
+// build task
+  grunt.registerTask('build', [
+    'useminPrepare',
+    'concat:generated',
+    'uglify:generated',
+    'copy',
+    'usemin'
+  ]);
+
+  var targets = ['*.html'];
+
+  grunt.config('useminPrepare', {
+    html: targets,
+    options: {
+      dest: 'gh-pages'
     }
   });
 
-// uglify
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.config('uglify', {
-    scripts: {
-      files: {
-        'gh-pages/javascripts/crr.js': 'tmp/crr.js'
-      }
+  grunt.config('copy', {
+    html: {
+      src: targets,
+      dest: 'gh-pages/'
+    },
+    css: {
+      src: 'crrlib.css',
+      dest: 'gh-pages/stylesheets/'
     }
+  });
+
+  grunt.config('usemin', {
+    html: targets.map(function(v) { return 'gh-pages/' + v; }),
   });
 
 };
